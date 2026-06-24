d18766faa6ef65781e19fe49e0a032e9f62d8a6b
sytux-boop <sattu.santitt@gmail.com>
2026-06-23 21:58:48 -0300

    Não sei mais o que eu tô fazendo

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const dotenv = require("dotenv").config()
+const cors = require("cors")
 
 const pool = require("./db")
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3040
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(express.static(path.join(__dirname, "..", "public/")))
 
 app.get("/", (req, res) => {
